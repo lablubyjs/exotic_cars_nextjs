cd36a7d6cf62772d88e1c1ea617d383728408ab8
Crislâine Santos <criis.santos@outlook.com>
2022-05-11 20:46:15 -0300

    feat: types added

---
+++ src/shared/types/index.ts
@@ -0,0 +1,42 @@
+import React, {MouseEventHandler} from 'react';
+
+import {ICar, IModel} from '@shared/interfaces';
+
+export type ChildrenProp = {
+    children: React.ReactNode;
+};
+
+export type ButtonProps = {
+    width: number;
+    height: number;
+    backgroundColor: string;
+    borderColor?: string;
+    borderWidth?: number;
+    borderRadius?: number;
+    children: React.ReactNode;
+    onClick?: MouseEventHandler<HTMLButtonElement>;
+    className?: string;
+};
+
+export type CarListProps = {
+    cars: ICar[];
+};
+
+export type CarsSlice = {
+    currentModel: ICar;
+    currentColorIndex: number;
+    pending: boolean;
+    error: boolean;
+};
+
+export type CarApiResponseData = {
+    car?: any;
+    cars?: any;
+    message: string;
+};
+
+export type CarDetailsProps = {
+    car: ICar;
+    currentColorIndex: number;
+    setCurrentColorIndex: (index: number) => void;
+};
