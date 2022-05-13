7257a53f6c9c11001341977727963e35efe15bb2
Crislâine Santos <criis.santos@outlook.com>
2022-05-13 15:27:34 -0300

    feat: import and export components added

---
+++ src/components/index.ts
@@ -0,0 +1,11 @@
+import Button from './UI/Buttons/Button';
+import ButtonCircle from './UI/Buttons/ButtonCircle';
+
+import CarsList from './CarsList';
+import CarDetails from './CarDetails';
+
+import Layout from './Layout';
+
+import Loading from './UI/Loading';
+
+export {Button, ButtonCircle, CarsList, CarDetails, Layout, Loading};
